9208a011652473dce9b88786cb12701e019b6614
Michael.K <michael34435@capslock.tw>
2018-12-22 06:55:19 +0800

    fix: regex pattern

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = () => async (ctx, next) => {
   const userAgent = ctx.req.headers['user-agent'];
 
-  if (/GoogleHC\/\d+\.\d+/.test(userAgent)) {
+  if (/^GoogleHC\/\d+\.\d+$/.test(userAgent)) {
     ctx.status = 200;
     ctx.body = {
       status: 'ok',
